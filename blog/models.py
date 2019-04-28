b00fd53a4c06d964ca21105d7ed7e90dfe2fbad3
msmelo <jvrmelo20@gmail.com>
2019-04-27 22:54:47 -0300

    My Blog app, first commit

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-        default=timezone.now)
+            default=timezone.now)
     published_date = models.DateTimeField(
-        blank=True, null=True)
+            blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,5 +16,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
 
 # Create your models here.
--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!^qp+b--nk811j*bp=%*#b8y_%_qq@sxp*k3(-7*$o=%!c+o@g'
+SECRET_KEY = '3y47-r%7=mvu%fp1q885at_ct1((&na($-u!vtisg2gl&m$3j&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
